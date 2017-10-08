024e5d2e4980633781c95b3edfe091327eaf5c5c
Donggyun Kim <donggyun@Donggyuns-MacBook-Pro.local>
2017-10-07 21:40:00 -0700

    hw03: updated 2017-10-07

--- hw03/code/make-teams-table.R
+++ hw03/code/make-teams-table.R
@@ -6,7 +6,7 @@
 # Input: data file 'nba2017-roster.csv' , 'nba2017-stats.csv'
 # Output: data file 'nba2017-teams.csv'
 # Author: Donggyun Kim
-# Date: 2017-10-06
+# Date: 2017-10-07
 # =============================================================
 
 # set working directory
@@ -21,13 +21,19 @@ library(ggplot2)
 roster <- read_csv("./data/nba2017-roster.csv")
 stats <- read_csv("./data/nba2017-stats.csv")
 
+# replace NA to 0
+stats$points3_perc[is.na(stats$points3_perc)] <- 0
+stats$points2_perc[is.na(stats$points2_perc)] <- 0
+stats$points1_perc[is.na(stats$points1_perc)] <- 0
+
+
 # add new variable
-stats %>% mutate(missed_fg = field_goals_atts - field_goals_made) # missed field goals
-stats %>% mutate(missed_ft =) # missed free throws
-stats %>% mutate(points = 3*points3_made + 2*points2_made + free_throw_made) # total points
-stats %>% mutate(rebounds = off_rebounds + def_rebounds) # rebounds
-stats %>% mutate((efficiency = points + rebounds + assists + steals + blocks
-                 - missed_fg - missed_ft - turnovers) / games_played) # efficiency
+stats <- stats %>% mutate(missed_fg = field_goals_atts - field_goals_made) # missed field goals
+stats <- stats %>% mutate(missed_ft = points1_atts - points1_made) # missed free throws
+stats <- stats %>% mutate(points = 3*points3_made + 2*points2_made + points1_made) # total points
+stats <- stats %>% mutate(rebounds = off_rebounds + def_rebounds) # rebounds
+stats <- stats %>% mutate(efficiency = (points + rebounds + assists + steals + 
+                             blocks - missed_fg - missed_ft - turnovers) / games_played) # efficiency
 
 # make summary
 sink("./output/effiency-summary.txt")
@@ -37,4 +43,36 @@ sink()
 # merge two data frames
 roster_stats <- roster %>% 
   inner_join(stats)
+
+# creat a data frame, teams
+teams <- roster_stats %>%
+  group_by(team) %>%
+  summarise(weight = round(mean(weight), 2), height = round(mean(height), 2), experience = sum(experience),
+            salary = round(sum(salary) / 1000000, 2), age = sum(age), 
+            points3 = sum(points3_made), points2 = sum(points2_made),
+            free_throws = sum(points1_made), points = sum(points), 
+            points3_perc = mean(points3_perc), points2_perc = mean(points2_perc),
+            points1_perc = mean(points1_perc), off_rebounds = sum(off_rebounds), 
+            def_rebounds = sum(def_rebounds), assists = sum(assists), 
+            steals = sum(steals), blocks = sum(blocks), turnovers = sum(turnovers),
+            fouls = sum(fouls), efficiency = sum(efficiency))
+
+# summary text file
+sink("./output/teams-summary.txt")
+summary(teams)
+sink()
+
+# export teams as a csv file
+write_csv(teams, "./data/nba2017_teams.csv")
+
+# save a star plot of teams
+pdf("./images/teams_star_plot.pdf")
+stars(teams[, -1], labels = teams$team)
+dev.off()
+
+# save a scatterplot of experience and salary
+pdf("./images/experience_salary.pdf")
+ggplot(data = teams, aes(x = experience, y = salary))+geom_point()+
+  geom_text(aes(label = teams$team), nudge_x = 2, nudge_y = 2)
+dev.off()
 
--- hw03/output/hw03-donggyun-kim.md
+++ hw03/output/hw03-donggyun-kim.md
@@ -1,14 +1,9 @@
----
-title: "HW03 - Ranking NBA Teams"
-author: "Donggyun Kim"
-date: "2017-10-07"
-output: github_document
----
+HW03 - Ranking NBA Teams
+================
+Donggyun Kim
+2017-10-07
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-```{r, message=FALSE}
+``` r
 setwd("~/Desktop/STAT 133/stat133-hws-fall17/hw03/")
 library(readr)
 library(dplyr)
@@ -16,8 +11,10 @@ library(ggplot2)
 teams<- read_csv("./data/nba2017_teams.csv")
 ```
 
-# Basic Rankings
-```{r}
+Basic Rankings
+==============
+
+``` r
 ggplot(teams, aes(x = reorder(team, salary), y = salary)) +
   geom_bar(stat='identity') +
   coord_flip() +
@@ -25,7 +22,9 @@ ggplot(teams, aes(x = reorder(team, salary), y = salary)) +
   labs(x = "Team", y = "Salary (in millions)", title = "NBA Teams ranked by Total Salary")
 ```
 
-```{r}
+![](hw03-donggyun-kim_files/figure-markdown_github-ascii_identifiers/unnamed-chunk-2-1.png)
+
+``` r
 ggplot(teams, aes(x = reorder(team, points), y = points)) +
   geom_bar(stat='identity') +
   coord_flip() +
@@ -33,10 +32,14 @@ ggplot(teams, aes(x = reorder(team, points), y = points)) +
   labs(x = "Team", y = "Total Points", title = "NBA Teams ranked by Total Points")
 ```
 
-```{r}
+![](hw03-donggyun-kim_files/figure-markdown_github-ascii_identifiers/unnamed-chunk-3-1.png)
+
+``` r
 ggplot(teams, aes(x = reorder(team, efficiency), y = efficiency)) +
   geom_bar(stat='identity') +
   coord_flip() +
   geom_hline(yintercept = mean(teams$efficiency), col = "Red") +
   labs(x = "Team", y = "Total Efficiency", title = "NBA Teams ranked by Total Efficiency")
 ```
+
+![](hw03-donggyun-kim_files/figure-markdown_github-ascii_identifiers/unnamed-chunk-4-1.png)
